89bf5aaf3d2016bb20c1acd907d23073e775bc21
abel <nahuel_abel@hotmail.com>
2023-04-21 05:43:46 -0300

    ultimos cambios

--- src/app/components/about/edit-acerca-de.component.ts
+++ src/app/components/about/edit-acerca-de.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { usuario } from 'src/app/model/usuario.model';
+import { ImageService } from 'src/app/service/image.service';
 import { UsuarioService } from 'src/app/service/usuario.service';
 
 @Component({
@@ -11,30 +12,32 @@ import { UsuarioService } from 'src/app/service/usuario.service';
 export class EditAcercaDeComponent implements OnInit {
   usuario: usuario = null;
 
-  constructor(private activatedRouter: ActivatedRoute, private usuarioService: UsuarioService, private router: Router) { }
+  constructor(private activatedRouter: ActivatedRoute, private usuarioService: UsuarioService, private router: Router, public imageService: ImageService) { }
 
   ngOnInit(): void {
     const id = this.activatedRouter.snapshot.params['id'];
     this.usuarioService.detail(id).subscribe(data => {
       this.usuario = data;
     }, err => {
-      alert("Error al modificar");
+      alert("ERROR AL MODIFICAR");
       this.router.navigate(['']);
     });
   }
 
   onUpdate(): void {
     const id = this.activatedRouter.snapshot.params['id'];
+    this.usuario.img = this.imageService.url
     this.usuarioService.update(id, this.usuario).subscribe(data => {
       this.router.navigate(['']);
     }, err => {
-      alert("Error al modificar la educacion");
+      alert("ERROR AL MODIFICAR EDUCACION");
       this.router.navigate(['']);
     });
   }
-  }
 
-  upLoadImage($event: any) {
-
+  uploadImage($event: any) {
+    const id = this.activatedRouter.snapshot.params['id'];
+    const name = "perfil_" + id;
+    this.imageService.uploadImage($event, name)
   }
 }
---
+++ src/app/service/image.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { Storage, list, ref, uploadBytes } from '@angular/fire/storage'
+import { getDownloadURL } from '@firebase/storage';
+import { async } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ImageService {
+  url: string = "";
+
+  constructor(private storage: Storage) { }
+
+  public uploadImage($event: any, name: string) {
+    const file = $event.target.files[0]
+    const imgRef = ref(this.storage, 'imagen/' + name)
+    uploadBytes(imgRef, file)
+      .then(Response => {
+        this.getImages()
+      })
+      .catch(error => console.log(error));
+  }
+
+  getImages(){
+    const imagesRef = ref(this.storage, 'imagen');
+    list(imagesRef)
+    .then(async Response => {
+      for(let item of Response.items){
+        this.url = await getDownloadURL(item);
+      }
+    })
+    .catch(error => console.log(error))
+  }
+}
