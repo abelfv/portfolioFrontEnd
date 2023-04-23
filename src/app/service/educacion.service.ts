6d00d190abbbd4746871850d66e61d0b24a46384
abel <nahuel_abel@hotmail.com>
2023-04-23 08:21:04 -0300

    Nuevos cambios

--- src/app/service/educacion.service.ts
+++ src/app/service/educacion.service.ts
@@ -14,7 +14,7 @@ export class EducacionService {
   constructor(private httpClient : HttpClient) { }
 
   public lista(): Observable<Educacion[]>{
-    return this.httpClient.get<Educacion[]>(this.URL + 'lista');
+    return this.httpClient.get<Educacion[]>(this.URL + '/lista');
   }
 
   public detail(id: number): Observable<Educacion>{
